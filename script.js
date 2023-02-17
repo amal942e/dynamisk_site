1cad4fba3aab2d56f9e1a357c77e902600f15962
Oliver Mackenzie <oliv295x@stud.kea.dk>
2023-02-17 10:03:57 +0100

    lavet forside

---
+++ script.js
@@ -0,0 +1,9 @@
+// 1: Vælg <template>'s content
+const templateElement = document.querySelector("#minTemplate").content;
+
+function visBil(bil) {
+    const klon = skabelon.cloneNode(true);
+    klon.querySelector("h1").textContent = bil.brand;
+    klon.querySelector("h2").textContent = bil.model;
+    document.querySelector("main").appendChild(klon);
+}
