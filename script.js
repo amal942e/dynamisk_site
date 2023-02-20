57cd6b5f8e3f6e3b468d4dab9015dcffa4bb6192
Oliver Mackenzie <oliv295x@stud.kea.dk>
2023-02-20 12:45:33 +0100

    burger menu fixet forside og andet

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+window.addEventListener("load", loadPage)
+
+/* konstant "burger" er selectoren til klassen .burger - samme med nav */
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".body-nav");
+
+
+
+const box = document.querySelector(".gridbox")
+const boxlink = document.querySelector(".gridlink")
+
+function loadPage() {
+    burger.addEventListener("click", toggleMenu)
+
+}
+
+/* toggle giver nu klassen show, hvis den ikke er der, og fjerner den igen hvis den er */
+function toggleMenu(){
+    nav.classList.toggle("show")
+
+    
+    /* document.querySelector("main").classList.toggle("disapear") */
+}
+
 // 1: Vælg <template>'s content
 const templateElement = document.querySelector("#minTemplate").content;
 
@@ -7,5 +31,7 @@ function visBil(bil) {
     klon.querySelector("h2").textContent = bil.model;
     document.querySelector("main").appendChild(klon);
 }
+
+
 
 
