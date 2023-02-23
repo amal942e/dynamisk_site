7ecf1dff5e55d91f514a8e7581d5536a5495598c
Amalie Sarfelt <amal942e@stud.kea.dk>
2023-02-23 10:41:58 +0100

    style

--- list.js
+++ list.js
@@ -1,3 +1,4 @@
+// variabler for parametre vi finder fra HTML
 const urlParams = new URLSearchParams(window.location.search);
 const by = urlParams.get("by");
 
@@ -5,8 +6,11 @@ const main = document.querySelector("main");
 const template = document.querySelector("template").content;
 document.querySelector("h2").textContent = by;
 
+// URL til categorilisten for vores byer
+
 const url = `https://rejsegalleri-07a8.restdb.io/rest/kontinenter?q={"by":"${by}"}`;
 console.log(url);
+//Variabler for billeder + RestDB nøgle
 const medieurl = "https://rejsegalleri-07a8.restdb.io/image/";
 const ikonurl = "https://rejsegalleri-07a8.restdb.io/icon/";
 const options = {
@@ -14,21 +18,27 @@ const options = {
     "x-apikey": "63f487e9478852088da6850c",
   },
 };
-
+// grab the data
 async function hentData() {
   const responses = await fetch(url, options);
   const json = await responses.json();
   vis(json);
 }
+// loope
+// for hver
 console.log(by.icon);
 function vis(json) {
   console.log(json);
   json.forEach((by) => {
+    // 1. fange template
+    // 2. clone
     const klon = template.cloneNode(true);
+    // 3. skifte data
     klon.querySelector(".stedfoto").src = "billeder/" + by.image;
     klon.querySelector(".ikon").src = "grafik/" + by.icon;
     klon.querySelector(".navn").textContent = by.titel;
     klon.querySelector("a").href += by._id;
+    // 4. appende
     main.appendChild(klon);
   });
 }
--- single.js
+++ single.js
@@ -1,19 +1,21 @@
+// URL vi finder til enkelt element
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
 const url = `https://rejsegalleri-07a8.restdb.io/rest/kontinenter/${id}`;
-
+// RestDB nøgle
 const options = {
   headers: {
     "x-apikey": "63f487e9478852088da6850c",
   },
 };
-
+// grab the data/fetch
 function hentData(url) {
   fetch(url, options)
     .then((res) => res.json())
     .then(vis);
 }
 function vis(element) {
+  // Skifte data
   document.querySelector(".navn").textContent = element.titel;
   document.querySelector(".bynavn").textContent = element.by;
   document.querySelector(".land").textContent = element.land;
